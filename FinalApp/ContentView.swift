a47c6d0939fd000c25b4a73fbf84bdf49b502a51
Brayden Imada <bi1012440@gmail.com>
2026-04-27 08:33:27 -0500

    Initial Commit

---
+++ FinalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalApp
+//
+//  Created by IMADA, BRAYDEN on 4/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalApp/FinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalAppApp.swift
+//  FinalApp
+//
+//  Created by IMADA, BRAYDEN on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
