3e98bca4309d398c8221794be7c860efaa00379a
Brayden-I <bi1012440@otc.edu>
2026-04-29 18:14:55 +0000

    Refactor Pokemon model to include additional properties and improve structure

--- FinalApp/PokemonModel.swift
+++ FinalApp/PokemonModel.swift
@@ -1,50 +1,75 @@
 //
-//  Pokemon.swift
+//  PokemonModel.swift
 //  FinalApp
 //
 //  Created by IMADA, BRAYDEN on 4/27/26.
 //
 
+import Foundation
 
-
-import Foundation // Has all the dependencies for the API
-
-// This is a struct. It is a datatype used for holding a value type. In this case we will be holding a pokemon reference
+// This struct holds the full details for a single Pokemon fetched from the detail endpoint
 struct Pokemon: Codable {
+    var id: Int
     var sprites: PokemonSprites
     var name: String
-    var base_experience: Int
-    var abilities: [PokemonAbility]
-    var moves: [PokemonMove]
-    var stats: [PokemonStats]
-    var height: Int
-    var weight: Int
+    var height: Int // In decimeters, divide by 10 for meters
+    var weight: Int // In hectograms, divide by 10 for kg
+    var types: [PokemonTypeSlot]
+    var stats: [PokemonStat]
 }
 
-// This is also a struct that will be used to hold the sprites from the API. As of now we will only use 1 sprite
+// Holds the sprite URLs. We only use the front facing one for now
 struct PokemonSprites: Codable {
     var front_default: String
 }
 
-struct PokemonAbility: Codable {
-    var ability: String
-}
-
-struct PokemonMove: Codable {
-    var move: String
-}
-
-struct PokemonStats: Codable {
-    var stat: String
-    var base_stat: Int
+// The API nests type name inside a slot object and then a type object
+struct PokemonTypeSlot: Codable {
+    var type: PokemonType
 }
 
 struct PokemonType: Codable {
-    var slot: Int
-    var type: String
+    var name: String
 }
 
-// This is the mock sample I will use for testing
+// Each stat has a value and a name
+struct PokemonStat: Codable {
+    var base_stat: Int
+    var stat: StatInfo
+}
+
+struct StatInfo: Codable {
+    var name: String
+}
+
+// This struct is what the list endpoint returns — just a name and a URL to fetch the full details
+struct PokemonListEntry: Codable, Identifiable {
+    var name: String
+    var url: String
+
+    // Identifiable requires an id. We pull it out of the URL since the API doesnt give us one directly
+    var id: String { name }
+}
+
+// This struct wraps the list endpoint response which puts the results inside a "results" key
+struct PokemonListResponse: Codable {
+    var results: [PokemonListEntry]
+}
+
+// Sample data used for previews so we dont have to hit the API every time
 extension Pokemon {
-    // TODO: Create sample
+    static let sample = Pokemon(
+        id: 25,
+        sprites: PokemonSprites(front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"),
+        name: "pikachu",
+        height: 4,
+        weight: 60,
+        types: [PokemonTypeSlot(type: PokemonType(name: "electric"))],
+        stats: [
+            PokemonStat(base_stat: 35, stat: StatInfo(name: "hp")),
+            PokemonStat(base_stat: 55, stat: StatInfo(name: "attack")),
+            PokemonStat(base_stat: 40, stat: StatInfo(name: "defense")),
+            PokemonStat(base_stat: 90, stat: StatInfo(name: "speed")),
+        ]
+    )
 }
